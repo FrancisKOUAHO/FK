da82d042bffeb4b78d9ebb42881d52422bf1ea11
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-10-24 15:36:19 +0200

    fnissh portfolio

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,35 +1,24 @@
 import NavBar from "./NavBar.tsx";
-import {FunctionComponent, useState} from "react";
+import {FunctionComponent} from "react";
 import HeaderProps from "../../type/HeaderProps.ts";
-import Hamburger from "./Hamburger.tsx";
 
 const Header: FunctionComponent<HeaderProps> = ({customStyle}) => {
-    const [showNavbar, setShowNavbar] = useState(false)
-
-    const handleShowNavbar = (e: any): void => {
-        e.preventDefault()
-        setShowNavbar((prevShowNavbar) => !prevShowNavbar);
-    }
 
     return (
         <header className="c-main-nav" style={customStyle}>
             <img src="/Francis.png" alt="" width={40}/>
 
-            <div className="c-main-nav__hamburger">
-                <img src="/icons/hamburger.svg" alt="" width={30} onClick={handleShowNavbar}/>
-                <Hamburger showNavbar={showNavbar} handleShowNavbar={handleShowNavbar}/>
-            </div>
 
             <NavBar/>
 
-            <div className="c-main-nav__sociaux">
+            {/*<div className="c-main-nav__sociaux">
                 <a href="https://www.linkedin.com/in/francis-kouaho/" target="_blank">
                     <img src="/icons/LinkedIn.svg" alt="" width={25}/>
                 </a>
                 <a href="https://twitter.com/KouahoFrancis" target="_blank">
                     <img src="/icons/twitter.svg" alt="" width={25}/>
                 </a>
-            </div>
+            </div>*/}
         </header>
     )
 }
--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,41 +1,11 @@
-import {FunctionComponent, useEffect, useState} from "react";
+import {FunctionComponent} from "react";
 
 const NavBar: FunctionComponent = () => {
-    const [activeSection, setActiveSection] = useState<string>('');
-
-    const handleScroll = () => {
-        const sections = document.querySelectorAll('section');
-        sections.forEach((section) => {
-            const rect = section.getBoundingClientRect();
-            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-                setActiveSection(section.id);
-            }
-        });
-    };
-
-    const handleHashChange = () => {
-        setActiveSection(window.location.hash.slice(1));
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        window.addEventListener('hashchange', handleHashChange);
-        handleScroll();
-        handleHashChange();
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-            window.removeEventListener('hashchange', handleHashChange);
-        };
-    }, []);
 
     return (
         <nav>
             <ul>
-                <li className={activeSection === 'home' ? 'active' : ''}><a href="#home">Accueil</a></li>
-                <li className={activeSection === 'skills' ? 'active' : ''}><a href="#skills">Compétences</a></li>
-                <li className={activeSection === 'projects' ? 'active' : ''}><a href="#projects">Projets</a></li>
-                <li className={activeSection === 'about' ? 'active' : ''}><a href="#about">À propos</a></li>
+                <li><a href="">Contacter-moi</a></li>
             </ul>
         </nav>
     )
