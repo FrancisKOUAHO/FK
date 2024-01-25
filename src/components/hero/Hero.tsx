9b59eb2c388ac1d4d9ddb3a81824030f459dc029
Francis KOUAHO <f.kouaho@skipperndt.com>
2024-01-25 22:32:15 +0100

    feat(front): display block

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,31 +1,11 @@
 import Header from "../header/Header.tsx";
-import {FunctionComponent, useState} from "react";
+import {FunctionComponent} from "react";
 import {Canvas} from "@react-three/fiber";
 import Experience from "../experience/Experience.tsx";
 
 const Hero: FunctionComponent = () => {
-    // quand on clique sur le bouton, ca fait apparaitre le texte (c-hero__content)
-    const [show, setShow] = useState(true)
-
-    const handleClick = () => {
-        setShow(!show)
-    }
-
     return (
         <div className="c-hero">
-            <button className="rotatingText" onClick={handleClick}>
-                <svg viewBox="0 0 200 200" width="150" height="150">
-                    <defs>
-                        <path id="circle" d="M 100, 100 m -75, 0 a 75, 75 0 1, 0 150, 0 a 75, 75 0 1, 0 -150, 0"></path>
-                    </defs>
-                    <text x="0" y="0">
-                        <textPath href="#circle" className="text">
-                            ☺️☺️☺️ - Cliquez ici pour en savoir plus ! - ☺️☺️☺️
-                        </textPath>
-                    </text>
-                </svg>
-            </button>
-
             <Canvas
                 flat
                 camera={{position: [0, 2, 5], fov: 40}}
@@ -39,7 +19,7 @@ const Hero: FunctionComponent = () => {
 
                 <h1>Francis <span>KOUAHO</span></h1>
 
-                <div className={`c-hero__content ${show ? 'hidden' : ''}`}>
+                <div className="c-hero__content">
                     <h2>Bonjour !</h2>
                     <p>Je suis Francis Kouaho, un développeur Front-End passionné par la création d'expériences web
                         exceptionnelles. Mon code est l'intersection de la créativité et de la précision, donnant
